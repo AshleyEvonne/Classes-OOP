94282e22b90b7e9097b8e14363c290702aef29dc
AshleyEvonne <ashleyevonne345@gmail.com>
2024-06-24 21:28:49 -0400

    Making new instances

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ class Animals {
         return[this.name, this.weight, this.height];
     }
 }
+
+const Dog = new Animals("Chow", 15, "2'11");
+console.log(Dog.dispaly());
+console.log(Dog);
+
+const Fish = new Animals("fishy", 2, "1");
+console.log (Fish.dispaly());
+console.log(Fish);
+
+const lion = new Animals("King", 200, "6");
+console.log(lion.dispaly());
+console.log(lion);
+
+const bird = new Animals("owl", 20, "4");
+console.log(bird.dispaly());
+console.log(bird);
